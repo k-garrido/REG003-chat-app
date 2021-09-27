37a66519b774f77573eb27d3f7c9e7ff1a4f3543
k-garrido <katherinne.g@hotmail.com>
2021-09-27 13:54:46 -0300

    pruebas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const auth = require('./auth');
 
 const root = (app, next) => {
   const pkg = app.get('pkg');
-  app.get('/', (req, res) => res.json({ name: pkg.name, version: pkg.version }));
+  app.get('/', (req, res) => res.json({ name: pkg.name, version: pkg.version, msg:'hola mundo' }));
   app.all('*', (req, resp, next) => next(404));
   return next();
 };
